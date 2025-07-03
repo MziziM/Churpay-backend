9250284b20d3f0d77eb748d99e5ed1a974fe7366
Mzizi Mzwakhe <mzizimzwakhe@MZIZISs-MacBook-Air.local>
2025-07-03 03:22:27 +0200

    Configure backend for deployment and CORS

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.utils import secure_filename
+from flask_cors import CORS
+
+# Allow UAT domain
+CORS(app, origins=["https://uat.churpay.com"])
 
 # --- Config ---
 UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
